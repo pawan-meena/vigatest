039a18cf95619d3e0e3ef4ecdec9671c2a447c96
pawan-meena <pawanmeenazee@outlook.com>
2022-05-02 00:15:20 +0530

    check

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Login from './Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
