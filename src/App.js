3a9c65868eebd6ac181d9590398c5f065eadb9b0
pawan-meena <pawanmeenazee@outlook.com>
2022-05-03 17:55:02 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react';
-import { Routes ,Route } from 'react-router-dom';
 import Check from "./Components/Check/check";
 import Exam from "./Components/Exam/exam";
 import Ept from "./Components/Ept/ept";
@@ -7,23 +7,15 @@ import Settings from "./Components/Settings/settings";
 import Dashboard from "./Components/Dashboard/dashboard";
 function App() {
   return (
-    <Routes>
-    <Route exact path="/">
-    <Ept /> 
-    </Route>
-    <Route path="/bashboard">
-    <Dashboard />
-    </Route>
-    <Route path="/check">
-    <Check />
-    </Route>
-    <Route path="/exam">
-    <Exam />
-    </Route>
-    <Route path="/settings">
-    <Settings />
-    </Route>
-   </Routes>
+    <BrowserRouter>
+   <Routes>
+     <Route path="/vigatest" element={<Ept />}/>
+     <Route path="/vigatest/exam" element={<Exam />}/>
+     <Route path="/vigatest/dashboard" element={<Dashboard />}/>
+     <Route path="/vigatest/check" element={<Check />}/>
+     <Route path="/vigatest/settings" element={<Settings />}/>
+     </Routes>
+ </BrowserRouter>
   );
 }
 export default  App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/css.css';
 import App from './App';
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <>
+    <App />
+  </>,
   document.getElementById('root')
 );
